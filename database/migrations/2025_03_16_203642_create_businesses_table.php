c84195535826ae69bcb1883ecc9d0e4b6e089761
Ahmed Sunil <iahmednill@gmail.com>
2025-04-18 02:52:01 +0500

    Rename table to 'businesses' and adjust formatting.
    Updated the table name from 'business' to 'businesses' to align with naming conventions. Also made minor formatting changes for improved code readability and consistency.

--- database/migrations/2025_03_16_203642_create_businesses_table.php
+++ database/migrations/2025_03_16_203642_create_businesses_table.php
@@ -1,16 +1,17 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('business', function (Blueprint $table) {
+        Schema::create('businesses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('contact');
